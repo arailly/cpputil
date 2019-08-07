1a039defade690617a73c40fbcf2c8260044a1e3
arailly <yusuke.arailly@gmail.com>
2019-08-07 12:44:45 +0900

    :sparkles: add Object and Series

--- include/arailib.hpp
+++ include/arailib.hpp
@@ -7,32 +7,102 @@
 #include <iterator>
 #include <numeric>
 #include <cmath>
+#include <fstream>
+#include <sstream>
 
 namespace arailib {
 
-    template <class UnaryOperation, class Iterable>
-    Iterable fmap(UnaryOperation op, const Iterable &v) {
-        Iterable result;
-        std::transform(v.begin(), v.end(), std::back_inserter(result), op);
-        return result;
+template <class UnaryOperation, class Iterable>
+Iterable fmap(UnaryOperation op, const Iterable &v) {
+    Iterable result;
+    std::transform(v.begin(), v.end(), std::back_inserter(result), op);
+    return result;
+}
+
+template <class Predicate, class Iterable>
+Iterable filter(Predicate pred, const Iterable &v) {
+    Iterable result;
+    std::copy_if(v.begin(), v.end(), std::back_inserter(result), pred);
+    return result;
+}
+
+template <class Iterable>
+double l2_norm(const Iterable& v1, const Iterable& v2) {
+    double result = 0;
+    for (size_t i = 0; i < v1.size(); i++) {
+        result += std::pow(v1[i] - v2[i], 2);
+    }
+    result = std::sqrt(result);
+    return result;
+}
+
+struct Object {
+    size_t index;
+    std::vector<double> x;
+
+    Object(size_t i, std::vector<double> v) {
+        index = i;
+        std::copy(v.begin(), v.end(), std::back_inserter(x));
     }
 
-    template <class Predicate, class Iterable>
-    Iterable filter(Predicate pred, const Iterable &v) {
-        Iterable result;
-        std::copy_if(v.begin(), v.end(), std::back_inserter(result), pred);
-        return result;
+    Object(std::vector<double> v) {
+        index = 0;
+        std::copy(v.begin(), v.end(), std::back_inserter(x));
     }
 
-    template <class Iterable>
-    double l2_norm(const Iterable& v1, const Iterable& v2) {
-        double result = 0;
-        for (int i = 0; i < v1.size(); i++) {
-            result += std::pow(v1[i] - v2[i], 2);
-        }
-        result = std::sqrt(result);
-        return result;
+    double& operator[] (size_t i) { return x[i]; }
+    const double& operator[] (size_t i) const { return x[i]; }
+
+    bool operator==(const Object& o) const {
+        if (index == o.index) return true;
+        return false;
     }
+
+    bool operator!=(const Object& o) const {
+        if (index != o.index) return true;
+        return false;
+    }
+
+    size_t size() const { return x.size(); }
+    auto begin() const { return x.begin(); }
+    auto end() const { return x.end(); }
+
+    void show() const {
+        std::cout << index << ": ";
+        for (const auto& xi : x) {
+            std::cout << xi << ' ';
+        } std::cout << std::endl;
+    }
+};
+
+typedef std::vector<Object> Series;
+
+auto split(std::string& input, char delimiter=',') {
+    std::istringstream stream(input);
+    std::string field;
+    std::vector<double> result;
+
+    while (std::getline(stream, field, delimiter)) {
+        result.push_back(std::stod(field));
+    }
+
+    return result;
+}
+
+Series read_csv(const std::string& path, const size_t& nrows=-1,
+                 const bool& skip_header=false) {
+    std::ifstream ifs(path);
+    std::string line;
+
+    Series series;
+    for (size_t i = 0; (i < nrows) && std::getline(ifs, line); ++i) {
+        // if first line is the header then skip
+        if (skip_header && (i == 0)) continue;
+        std::vector<double> v = split(line);
+        series.push_back(Object(i, v));
+    }
+    return series;
+}
 
 }
 
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -7,13 +7,15 @@
 #include "gtest/gtest.h"
 #include "arailib.hpp"
 
+#include <unistd.h>
+
 TEST(Functional, fmap_test) {
     std::vector<int> v{1, 2, 3};
     const auto double_func = [](int x) { return x * 3; };
     const auto actual = arailib::fmap(double_func, v);
 
     const std::vector<int> expect{3, 6, 9};
-    EXPECT_EQ(actual, expect);
+    ASSERT_EQ(actual, expect);
 }
 
 TEST(Functional, filter_test) {
@@ -22,7 +24,7 @@ TEST(Functional, filter_test) {
     const auto actual = arailib::filter(pred, v);
 
     const std::vector<int> expect{4, 5};
-    EXPECT_EQ(actual, expect);
+    ASSERT_EQ(actual, expect);
 }
 
 TEST(Utilities, l2_norm_test) {
@@ -31,5 +33,26 @@ TEST(Utilities, l2_norm_test) {
     const double actual = arailib::l2_norm(v1, v2);
 
     const double expect = 5;
-    EXPECT_EQ(actual, expect);
+    ASSERT_EQ(actual, expect);
+}
+
+TEST(Object, method_test) {
+    const arailib::Object o1(0, {1, 2, 3});
+    ASSERT_EQ(o1.index, 0);
+    ASSERT_EQ(o1[1], 2);
+
+    arailib::Object o2(0, {1, 2, 3});
+    ASSERT_TRUE(o1 == o2);
+
+    o2.index++;
+    ASSERT_FALSE(o1 == o2);
+}
+
+TEST(Series, read_csv_test) {
+    const std::string data_path = "../../../test/data/series.csv";
+    const arailib::Series series = arailib::read_csv(data_path);
+    const arailib::Object actual = series[1];
+
+    arailib::Object expect(1, {2, 3, 4});
+    ASSERT_EQ(actual, expect);
 }
