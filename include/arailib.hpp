3b40d2fdfb80c1d4fa84370ce02cdb2c65e64cbc
arailly <yusuke.arailly@gmail.com>
2019-08-07 09:59:07 +0900

    :bug: refactor

--- include/arailib.hpp
+++ include/arailib.hpp
@@ -1,18 +1,22 @@
 #ifndef ARAILIB_ARAILIB_HPP
 #define ARAILIB_ARAILIB_HPP
 
-template <typename UnaryOperation, typename Iterable>
-Iterable fmap(UnaryOperation op, Iterable& v) {
-    Iterable result;
-    std::transform(v.begin(), v.end(), std::back_inserter(result), op);
-    return result;
-}
+namespace arailib {
 
-template <typename Predicate, typename Iterable>
-Iterable filter(Predicate pred, Iterable& v) {
-    Iterable result;
-    std::copy_if(v.begin(), v.end(), std::back_inserter(result), pred);
-    return result;
+    template<class UnaryOperation, class Iterable>
+    Iterable fmap(UnaryOperation op, Iterable &v) {
+        Iterable result;
+        std::transform(v.begin(), v.end(), std::back_inserter(result), op);
+        return result;
+    }
+
+    template<class Predicate, class Iterable>
+    Iterable filter(Predicate pred, Iterable &v) {
+        Iterable result;
+        std::copy_if(v.begin(), v.end(), std::back_inserter(result), pred);
+        return result;
+    }
+
 }
 
 #endif //ARAILIB_ARAILIB_HPP
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -10,7 +10,7 @@
 TEST(Functional, fmap_test) {
     std::vector<int> v{1, 2, 3};
     const auto double_func = [](int x) { return x * 3; };
-    const auto actual = fmap(double_func, v);
+    const auto actual = arailib::fmap(double_func, v);
 
     const std::vector<int> expect{3, 6, 9};
     EXPECT_EQ(actual, expect);
@@ -19,7 +19,7 @@ TEST(Functional, fmap_test) {
 TEST(Functional, filter_test) {
     std::vector<int> v{1, 2, 3, 4, 5};
     const auto pred = [](int x) { return (x > 3); };
-    const auto actual = filter(pred, v);
+    const auto actual = arailib::filter(pred, v);
 
     const std::vector<int> expect{4, 5};
     EXPECT_EQ(actual, expect);
